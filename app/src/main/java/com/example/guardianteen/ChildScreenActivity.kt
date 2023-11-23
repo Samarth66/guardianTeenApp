a7c85f40d35d119039589217e9268bb9c588f7f7
deepaknayani22 <nayanideepakreddy@gmail.com>
2023-11-23 02:37:38 -0700

    Added SOS feature and device free fall

--- app/src/main/java/com/example/guardianteen/ChildScreenActivity.kt
+++ app/src/main/java/com/example/guardianteen/ChildScreenActivity.kt
@@ -1,11 +1,109 @@
 package com.example.guardianteen
 
+import FreeFallDetector
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+
+
+
+
 
 class ChildScreenActivity : AppCompatActivity() {
+
+    private lateinit var freeFallDetector: FreeFallDetector
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private val locationRequestCode = 101
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_child_screen)
+
+        freeFallDetector = FreeFallDetector(this) {
+            Log.d("FreeFallDetector", "Free Fall Detected!")
+        }
+
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+
+        val sosButton: Button = findViewById(R.id.sosButton)
+        sosButton.setOnClickListener {
+            checkLocationPermissionAndGetLocation()
+        }
     }
+
+    private fun checkLocationPermissionAndGetLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_COARSE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION),
+                locationRequestCode
+            )
+            return
+        }
+        getLastLocation()
+    }
+
+    private fun getLastLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+
+            return
+        }
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location ->
+                if (location != null) {
+                    val latitude = location.latitude
+                    val longitude = location.longitude
+                    Toast.makeText(this, "Latitude: $latitude, Longitude: $longitude", Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            locationRequestCode -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    getLastLocation()
+                }
+                return
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        freeFallDetector.startListening() // Start listening for free fall
+    }
+
+    override fun onPause() {
+        super.onPause()
+        freeFallDetector.stopListening() // Stop listening to save resources
+    }
+
+
 }
+
+
+
+
